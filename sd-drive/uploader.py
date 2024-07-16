1c3f6bf1198e19fd8cb034a3eafeb2efc368ff7b
seggs <68317833+gabszap@users.noreply.github.com>
2024-07-16 00:23:24 -0400

    Update uploader.py

--- sd-drive/uploader.py
+++ sd-drive/uploader.py
@@ -6,10 +6,12 @@ from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 
-
 def authenticate_google_drive():
     SCOPES = ['https://www.googleapis.com/auth/drive.file']
     creds = None
+    
+    # Caminho para o arquivo credentials.json no Kaggle
+    credentials_path = '/kaggle/input/your-dataset-name/credentials.json'
     
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
@@ -18,14 +20,13 @@ def authenticate_google_drive():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(credentials_path, SCOPES)
             creds = flow.run_local_server(port=0)
         
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
     
     return build('drive', 'v3', credentials=creds)
-
 
 def upload_to_drive(service, file_path, file_name, folder_id=None):
     file_metadata = {'name': file_name}
